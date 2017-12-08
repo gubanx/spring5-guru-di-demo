3fd99c8a3eb53ca3fb18e7f02d758bcec1ef732d
guban <gubanx@gmail.com>
2017-12-08 21:30:49 +0100

    @Profile

--- src/main/java/org/sample/didemo/service/PrimarySpanishGreetingService.java
+++ src/main/java/org/sample/didemo/service/PrimarySpanishGreetingService.java
@@ -2,16 +2,16 @@ package org.sample.didemo.service;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-@Controller
+@Service
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Controlador con Primary";
+        return "Saludo desde el perfil 'es'";
     }
 
 }
