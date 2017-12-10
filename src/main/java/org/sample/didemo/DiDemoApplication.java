442ead29d78c0ab4a7b2a5751af768f48dd8dd18
guban <gubanx@gmail.com>
2017-12-10 20:56:08 +0100

    Properties

--- src/main/java/org/sample/didemo/DiDemoApplication.java
+++ src/main/java/org/sample/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import org.sample.didemo.controller.ConstructorInjectedController;
 import org.sample.didemo.controller.GetterInjectedController;
 import org.sample.didemo.controller.MyController;
 import org.sample.didemo.controller.PropertyInjectedController;
+import org.sample.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,16 +12,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DiDemoApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("myController");
+        MyController controller = (MyController) ctx.getBean("myController");
 
-		controller.hello();
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean("fakeDataSource");
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getUrl());
 
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
-
-	}
+    }
 }
