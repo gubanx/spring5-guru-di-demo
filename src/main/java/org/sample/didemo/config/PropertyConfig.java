fc70eff83c1e607a43eb431884e567f4fe94de5c
guban <gubanx@gmail.com>
2017-12-10 21:04:23 +0100

    Variables de sistema operativo

--- src/main/java/org/sample/didemo/config/PropertyConfig.java
+++ src/main/java/org/sample/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package org.sample.didemo.config;
 
 import org.sample.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${db.user}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USER"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return  fakeDataSource;
